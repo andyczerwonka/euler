6ca670eca58f405bf97e3823cf3f9e8b5d60f11d
arcticpenguin <andy.czerwonka@gmail.com>
2011-01-26 09:17:26 -0700

    rewrote answer #3 to make it more readable

--- src/main/scala/euler/Euler003.scala
+++ src/main/scala/euler/Euler003.scala
@@ -5,10 +5,10 @@ class Euler003 {
   lazy val naturals: Stream[Int] = Stream.cons(2, naturals.map(_ + 1))
 
   def answer = {
-    var theNum = 600851475143L
+    var num = 600851475143L
     naturals.dropWhile(n => {
-      while (theNum % n == 0) {
-	theNum /= n }; theNum > 1
+      while (num % n == 0) {num = num / n};
+      num > 1
     }).head
   }
 
