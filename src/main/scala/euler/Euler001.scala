e0976952a6dbb314e765a9779a480c11ba007c8b
arcticpenguin <andy.czerwonka@gmail.com>
2011-01-26 07:46:20 -0700

    changed foldLeft to sum in the first and second cases

--- src/main/scala/euler/Euler001.scala
+++ src/main/scala/euler/Euler001.scala
@@ -2,7 +2,7 @@ package euler
 
 class Euler001 {
 
-  def filterApproach = (1 until 1000).view.filter(i => (i % 3 == 0 || i % 5 == 0)).foldLeft(0)(_ + _)
+  def filterApproach = (1 until 1000).view.filter(i => (i % 3 == 0 || i % 5 == 0)).sum
 
   def patternMatchApproach = (1 until 1000).foldLeft(0) { (total, x) =>
     x match {
--- src/main/scala/euler/Euler002.scala
+++ src/main/scala/euler/Euler002.scala
@@ -4,6 +4,6 @@ class Euler002 {
 
   lazy val fib: Stream[Int] = Stream.cons(0, Stream.cons(1, fib.zip(fib.tail).map(p => p._1 + p._2)))
 
-  def answer = fib.filter(_ % 2 == 0).takeWhile(_ < 4000000).foldLeft(0)(_ + _)
+  def answer = fib.filter(_ % 2 == 0).takeWhile(_ < 4000000).sum
 
 }
