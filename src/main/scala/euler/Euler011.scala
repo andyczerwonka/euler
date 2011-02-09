28a2564feda4d145e20f7537f392ab76f0f79e29
arcticpenguin <andy.czerwonka@gmail.com>
2011-02-08 19:15:09 -0700

    #11: progress... I thought the numbers are right, but the answer is still wrong... more debugging

--- src/main/scala/euler/Euler011.scala
+++ src/main/scala/euler/Euler011.scala
@@ -7,53 +7,67 @@ import scala.collection.mutable.ListBuffer
 
 class Euler011 extends Actor {
 
-  val forLater = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48".grouped(3).map(_.trim.toInt).grouped(20)
-
   type Matrix = Seq[Seq[Int]]
-  case class MatrixMaxRequest(matrix: Matrix, from: Actor)
-  case class MatrixMaxReply(matrix: Matrix, max: Int)
-  
-  def run() {
-    val input = "01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25"
-    val intArray = input split " " map (_.toInt) toList
-    val array2D = (intArray grouped 5 toList)
-    val sliced = array2D.map(row => row.sliding(3, 1).toList).sliding(3, 1).toList
-    val subMatrices: List[Matrix] = sliced flatMap (_.transpose)
-      for (matrix <- subMatrices) {
-        val calculator = new Calculator
-        calculator.start
-        calculator ! MatrixMaxRequest(matrix, this)
-      }
-  }
+  case class CalculateMaxRequest(matrix: Matrix, from: Actor)
+  case class CalculateMaxReply(matrix: Matrix, max: Int)
 
-  var total = 0
-  var count = 18
-  override def act = loop {
-    react {
-      case MatrixMaxReply(matrix, max) =>
-        println("Reply Received for " + matrix + " with " + max)
-		total = if (max > total) max else total
-		count -= 1
-		if (count == 0) {
-		  println("We're done - the answer is " + total)
-		  exit()
-		}
+  var count = 0
+
+  def execute() {
+    val input = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
+    val intArray = input split " " map (_.toInt) toList
+    val array2D = (intArray grouped 20 toList)
+    val sliced = array2D.map(row => row.sliding(4, 1).toList).sliding(4, 1).toList
+    val subMatrices: List[Matrix] = sliced flatMap (_.transpose)
+    count = subMatrices.size
+    println("We have " + count + " matrices to process")
+    for (matrix <- subMatrices) {
+      val calculator = new MaxProductCalculator
+      calculator.start()
+      calculator ! CalculateMaxRequest(matrix, this)
     }
   }
 
-  class Calculator extends Actor {
+  var total = 0
+  override def act() = loop {
+    react {
+      case CalculateMaxReply(matrix, max) =>
+        total = if (max > total) max else total
+        count -= 1
+        if (count == 0) {
+          println("We're done - the answer is " + total)
+          exit()
+        }
+    }
+  }
 
-    override def act = loop {
+  class MaxProductCalculator extends Actor {
+
+    override def act() = loop {
       react {
-        case MatrixMaxRequest(matrix, actor) =>
-          println("Request Received for " + matrix)
-          actor ! MatrixMaxReply(matrix, calculateMax(matrix))
-		  actor ! MatrixMaxReply(matrix.transpose, calculateMax(matrix.transpose))
+        case CalculateMaxRequest(matrix, actor) =>
+          actor ! CalculateMaxReply(matrix, calculateMax(matrix))
           exit()
       }
     }
 
-    def calculateMax(matrix: Matrix) = matrix.map(_ product).max
+    def calculateMax(matrix: Matrix) = {
+      val transposedMatrix = matrix.transpose
+      val horizontal = matrix.map(_ product).max
+      val vertical = transposedMatrix.map(_ product).max
+      val diagonalLeft = calculateDiagonal(matrix)
+      val diagonalRight = calculateDiagonal(transposedMatrix)
+      val products = horizontal :: vertical :: diagonalLeft :: diagonalRight :: Nil
+      products max
+    }
+
+    def calculateDiagonal(matrix: Matrix) = {
+      var diagonalTotal = 1
+      for (i <- 0 until matrix.size) {
+        diagonalTotal = diagonalTotal * matrix(i)(i)
+      }
+      diagonalTotal
+    }
 
   }
 
@@ -62,7 +76,7 @@ class Euler011 extends Actor {
 object Euler011Main {
   def main(args: Array[String]) {
     val e = new Euler011
-    e.start
-	e.run
+    e.start()
+    e.execute()
   }
 }
