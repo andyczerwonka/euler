a327b4008b35f5830c13c7b68b464ddae0a384c4
Andy Czerwonka <andy.czerwonka@gmail.com>
2012-01-04 20:40:23 -0700

    trying to get the tests to run under the Eclipse JUnit runner

--- src/test/scala/euler/EulerSpecs.scala
+++ src/test/scala/euler/EulerSpecs.scala
@@ -5,11 +5,14 @@ import org.specs.runner.JUnit4
 import org.specs.runner.ConsoleRunner
 import org.specs.matcher._
 import org.specs.specification._
+import org.junit.runner.RunWith
+import org.specs.runner.JUnitSuiteRunner
 
 class EulerSpecsAsTest extends JUnit4(EulerSpecs)
 object EulerSpecsSpecsRunner extends ConsoleRunner(EulerSpecs)
 
-object EulerSpecs extends Specification {
+@RunWith(classOf[JUnitSuiteRunner])
+object EulerSpecs extends SpecificationWithJUnit {
 
   "001: The sum of all the natural numbers below one thousand that are multiples of 3 or 5 is 233168" in {
     val e1 = new Euler001
