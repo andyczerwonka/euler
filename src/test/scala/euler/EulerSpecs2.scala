a969cdd5e48d0e627e38534b8d6c9482e2071754
Andy Czerwonka <andy.czerwonka@gmail.com>
2012-01-09 19:28:00 -0700

    isPalindrome function for euler4.... now the algorithm to use it

--- src/test/scala/euler/EulerSpecs2.scala
+++ src/test/scala/euler/EulerSpecs2.scala
@@ -13,16 +13,41 @@ class EulerSpecs2 extends JUnitMustMatchers {
 
   @Test
   def testEuler022 {
-    
-    def wordValue(s:String, list:List[String]):Int = {
+
+    def wordValue(s: String, list: List[String]): Int = {
       val locationValue = list.indexOf(s) + 1
       val wordValue = s.map(_.toInt - 64).sum
       locationValue * wordValue
     }
-    
+
     val names = Euler022.sortedNames
     val sum = names.map(s => wordValue(s, names)).sum
     sum mustEqual 871198282
+
+  }
+
+  @Test
+  def testEuler004 {
+
+    def isPalindrome(n: Int): Boolean = {
+
+      def split(n: Int) = {
+        val stringValue = n.toString()
+        val left = stringValue.substring(0, stringValue.size / 2)
+        val right = stringValue.substring(stringValue.size / 2)
+        (left, right)
+      }
+
+      if (n.toString.size % 2 != 0)
+        false
+      else {
+        val (left, right) = split(n)
+        left.equals(right.reverse)
+      }
+    }
+
+    isPalindrome(9009) mustEqual true
+    isPalindrome(9008) mustEqual false
 
   }
 
